730578402803ab47951c9b17eea91e8c64e58028
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2025-09-06 15:01:11 +0300

    Updates

---
+++ create-favicon.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Creating favicon files for Eye Sight Optical...
+
+REM This batch file helps create the favicon files
+REM You can run this after opening generate-icons.html in your browser
+
+echo.
+echo Instructions:
+echo 1. Open public/generate-icons.html in your web browser
+echo 2. Right-click on each canvas and save as the suggested filename
+echo 3. Place the saved files in the public folder
+echo.
+echo Required files:
+echo - favicon.ico (32x32)
+echo - logo192.png (192x192) 
+echo - logo512.png (512x512)
+echo.
+echo The SVG versions are already created and will work as fallbacks.
+echo.
+pause
